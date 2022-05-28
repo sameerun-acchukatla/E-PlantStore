fd70d33381c9daf2ddb3e713a8d32d74ff9f726d
Sameerun1 <sameerun.shaikh@gmail.com>
2022-05-28 23:58:24 +0200

    fix ui issues

--- adminportal/src/main/java/com/adminportal/domain/security/Authority.java
+++ adminportal/src/main/java/com/adminportal/domain/security/Authority.java
@@ -3,6 +3,7 @@ package com.adminportal.domain.security;
 import org.springframework.security.core.GrantedAuthority;
 
 public class Authority implements GrantedAuthority{
+
 	private final String authority;
 	
 	public Authority(String authority) {
